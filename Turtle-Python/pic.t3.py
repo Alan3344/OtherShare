74f61b76463be68089d3a0c2514e4b7b3f32bc20
Xander-97 <60773912+Xander-97@users.noreply.github.com>
2020-02-11 13:20:07 +0800

    Add files via upload

---
+++ Turtle-Python/pic.t3.py
@@ -0,0 +1,23 @@
+# File Name : pic.t3
+# @date : 2020-02-04  13:20
+
+# PythonDraw.py
+
+import turtle
+turtle.hideturtle()
+
+turtle.setup(650, 350, 200, 200)
+turtle.penup()
+turtle.fd(-250)
+turtle.pendown()
+turtle.pensize(25)
+turtle.pencolor("purple")
+turtle.seth(-40)
+for i in range(4):
+    turtle.circle(40, 80)
+    turtle.circle(-40, 80)
+turtle.circle(40, 80/2)
+turtle.fd(40)
+turtle.circle(16, 180)
+turtle.fd(40 * 2/3)
+turtle.mainloop()
---
+++ Turtle-Python/pic.tree.py
@@ -0,0 +1,61 @@
+# File Name : pic.tree
+# @date : 2020-02-04  13:08
+
+from turtle import *
+import random
+import time
+# setup(500,500,50,50)   # 窗口位置
+n = 100.0
+
+hideturtle()  # 隐藏画笔
+
+speed("fastest")
+screensize(bg='seashell')
+left(90)
+forward(3*n)
+color("orange", "yellow")
+begin_fill()
+left(126)
+
+for i in range(5):
+    forward(n/5)
+    right(144)
+    forward(n/5)
+    left(72)
+end_fill()
+right(126)
+
+color("dark green")
+backward(n*4.8)
+def tree(d, s):
+    if d <= 0: return
+    forward(s)
+    tree(d-1, s*.8)
+    right(120)
+    tree(d-3, s*.5)
+    right(120)
+    tree(d-3, s*.5)
+    right(120)
+    backward(s)
+tree(15, n)
+backward(n/2)
+
+for i in range(200):
+    a = 200 - 400 * random.random()
+    b = 10 - 20 * random.random()
+    up()
+    forward(b)
+    left(90)
+    forward(a)
+    down()
+    if random.randint(0, 1) == 0:
+            color('tomato')
+    else:
+        color('wheat')
+    circle(2)
+    up()
+    backward(a)
+    right(90)
+    backward(b)
+
+time.sleep(60)
